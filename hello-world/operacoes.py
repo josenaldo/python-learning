08e79d737451e3f3d5a1cc712d784edd2d917513
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2019-10-29 10:53:19 +0000

    Descrição das operações e registro de algumas teclas de atalho

--- hello-world/operacoes.py
+++ hello-world/operacoes.py
@@ -2,15 +2,55 @@
 
 x = 25
 y = 5
-soma = x + y
-subtracao = x - y
-multiplicacao = x * y
-divisao = x / y
-potencia = x ** y
+z = 3
+w = -4
 
-print ( x, " + ", y, " = ", soma )
-print ( x, " - ", y, " = ", subtracao )
-print ( x, " * ", y, " = ", multiplicacao )
-print ( x, " / ", y, " = ", divisao )
-print ( x, " ** ", y, " = ", potencia )
+# A soma é feita com o operador +
+print("========== SOMA ==========")
+soma1 = x + y
+soma2 = x + z
+print ( x, " + ", y, " = ", soma1 )
+print ( x, " + ", z, " = ", soma2 )
 
+# A subtração é feita com o operador -
+print("\n========== SUBTRAÇÃO ==========")
+subtracao1 = x - y
+subtracao2 = z - x
+print ( x, " - ", y, " = ", subtracao1 )
+print ( z, " - ", x, " = ", subtracao2 )
+
+# A multiplicação é feita com o operador *
+print("\n========== MULTIPLICAÇÃO ==========")
+multiplicacao1 = x * y
+multiplicacao2 = x * w
+print ( x, " * ", y, " = ", multiplicacao1 )
+print ( x, " * ", w, " = ", multiplicacao2 )
+
+# A divisão é feita com o operador /
+# note que a divisão retora um número de ponto flutuante
+print("\n========== DIVISÃO ==========")
+divisao1 = x / y
+divisao2 = x / z
+print ( x, " / ", y, " = ", divisao1 )
+print ( x, " / ", z, " = ", divisao2 )
+
+# A divisão inteira é feita com o operador //
+print("\n========== DIVISÃO INTEIRA ==========")
+divisaoInteira1 = x // y
+divisaoInteira2 = x // z
+print ( x, " // ", y, " = ", divisaoInteira1 )
+print ( x, " // ", z, " = ", divisaoInteira2 )
+
+# O resto é feito com o operador %
+print("\n========== RESTO ==========")
+resto1 = x % y
+resto2 = x % z
+print ( x, " % ", y, " = ", resto1 )
+print ( x, " % ", z, " = ", resto2 )
+
+# A potência é feita com o operador **
+print("\n========== POTÊNCIA ==========")
+potencia1 = x ** y
+potencia2 = y ** w
+print ( x, " ** ", y, " = ", potencia1 )
+print ( y, " ** ", w, " = ", potencia2 )
--- semana1/README.md
+++ semana1/README.md
@@ -20,3 +20,13 @@ Artigos e vídeo úteis para complementar a semana 1.
 
 - O jogo da Imitação
 - Estrelas além do tempo
+
+## Atalhos do VSCode
+
+- `Alt + Cima` ou `Alt + Baixo` - move a linha para cima ou para baixo
+- `Alt + Shift + Cima` ou `Alt + Shift + Baixo` - copia a linha para cima ou para baixo
+- `Shift + enter` - Executa o código python no terminal
+
+
+
+es
